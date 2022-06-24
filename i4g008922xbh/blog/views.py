b436258f6aefb0d7689d220dcfe15b7c6d5e07e9
psalmuelle <bjoestores@gmail.com>
2022-06-24 12:43:22 +0100

    first commit

---
+++ i4g008922xbh/blog/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+# Create your views here.
+class PostListView():
+    pass
+
+
+class PostCreateView():
+    pass
+
+
+class PostDetailView():
+    pass
+
+
+class PostUpdateView():
+    pass
+
+
+class PostDeleteView():
+    pass
